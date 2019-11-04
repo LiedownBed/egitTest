a30e9189e77b37a33575b97965434d47c23d6aa4
LiedownBed <57317198+LiedownBed@users.noreply.github.com>
2019-11-04 22:17:27 +0800

    respond client
    hello client

--- src/dao/UserDao.java
+++ src/dao/UserDao.java
@@ -5,6 +5,7 @@ public class UserDao {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello server");
+		System.out.println("hello client");
 	}
 
 }
